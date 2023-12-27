a3f64cdedf9f8ac219b04b8fec352a53e3ec07b9
Dave Naugler <davenaugler@gmail.com>
2023-12-27 12:49:12 -0700

    🚀 Initial commit

---
+++ src/test/java/com/coderscampus/RecipeFinder/RecipeFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.RecipeFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
